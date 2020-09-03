8fc0eeaa65e2d26402aeeed2a60dac4af7bed867
salgokrisz <salgokrisz@gmail.com>
2020-09-03 15:43:51 +0200

    adding account recovery

--- MagicCardTrader/settings.py
+++ MagicCardTrader/settings.py
@@ -129,3 +129,12 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'Magic:index'
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+#EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_USER = 'salgokrisz@gmail.com'
+#EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
+EMAIL_HOST_PASSWORD = 'CX906ki1995'
